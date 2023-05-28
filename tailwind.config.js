aa4a4409cad0b46a2d686589d607e81bb77a398a
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-05-28 22:26:01 +0530

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,6 @@ module.exports = {
       dropShadow: {
         '3xl': '0px 0px 35px rgba(255, 255, 255, 40)',
       },
-      keyframes: {
-        wiggle: {
-          '0%, 100%': { transform: 'rotate(-3deg)' },
-          '50%': { transform: 'rotate(3deg)' },
-        }
-      },
-      animation: {
-        'translate': 'translate 3s linear infinite',
-      }
     },
   },
   plugins: [],
